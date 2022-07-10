889215e061cab73d19f4dd9c493f74c4229d5880
Enzo Gray <56325771+FroidoTheGreat@users.noreply.github.com>
2022-07-09 21:26:57 -0500

    commit

--- game/bin/sprites.lua
+++ game/bin/sprites.lua
@@ -13,9 +13,9 @@ function sprites.load(folder, loc)
 		return nil
 	elseif #all[1] >= 4 and string.sub(all[1], #all[1] - 3, #all[1]) == ".png" then
 		for i, f_name in ipairs(all) do -- look through folder
-			print(folder, f_name)
 			local img = lg.newImage(folder.."/"..f_name)
 			local num = tonumber(string.sub(f_name, 1, #f_name - 4))
+			print(folder, f_name, num)
 			loc[num] = img
 		end
 	else
